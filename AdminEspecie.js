71595ddd5cb0c9b6a1f3bbdc8644ae64bf474ff6
EblissYisse <segoviavenales@hotmail.com>
2023-04-05 07:16:36 -0500

    v1

---
+++ AdminEspecie.js
@@ -0,0 +1,15 @@
+//Esta clase va  a tener dos metodos segun el diagrama
+
+class AdminEspecie{
+
+    crearEspecie(req, res){
+
+    }
+
+    //metodo
+    listarEspecie(req, res){
+
+    }
+}
+
+module.exports=AdminEspecie;
---
+++ index.js
@@ -0,0 +1,19 @@
+//1.importamos express
+
+const express=require("express");
+const AdminEspecie=require("./AdminEspecie");
+
+class AteneaVetAPI{
+
+    constructor(){
+
+    }
+
+    //configurar los encabezados para CORS
+    configurarCORS(){
+
+    }
+    iniciarServidor(){
+        
+    }
+}
